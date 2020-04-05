6fbc4477812f83cf2b9edbd5fecfd69a77be1999
Luiz Américo <luizamericop@gmail.com>
2020-04-05 14:45:10 -0300

    update dependencies and general improvements

--- generators/app/templates/tools/build.js
+++ generators/app/templates/tools/build.js
@@ -7,7 +7,11 @@ const pkg = require('../package.json')
 
 let promise = Promise.resolve()
 
-let dependencies = Object.assign({}, pkg.dependencies || {}, pkg.peerDependencies || {})
+let dependencies = Object.assign(
+  {},
+  pkg.dependencies || {},
+  pkg.peerDependencies || {}
+)
 
 // Clean up the output directory
 promise = promise.then(() => del(['dist/*']))
@@ -20,21 +24,29 @@ const formats = ['es', 'umd']
 
 // Compile source code into a distributable format with Babel
 formats.forEach((format) => {
-  promise = promise.then(() => rollup.rollup({
-    input: 'src/index.js',
-    external: Object.keys(dependencies),
-    plugins: [babel({
-      exclude: 'node_modules/**'
-    })]
-  }).then(bundle => bundle.write({
-    file: `dist/${format === 'es' ? outputName : outputName + '.umd'}.js`,
-    format,
-    sourcemap: true,
-    name: format === 'umd' ? exportName : undefined
-  })))
+  promise = promise.then(() =>
+    rollup
+      .rollup({
+        input: 'src/index.js',
+        external: Object.keys(dependencies),
+        plugins: [
+          babel({
+            exclude: 'node_modules/**',
+          }),
+        ],
+      })
+      .then((bundle) =>
+        bundle.write({
+          file: `dist/${format === 'es' ? outputName : outputName + '.umd'}.js`,
+          format,
+          sourcemap: true,
+          name: format === 'umd' ? exportName : undefined,
+        })
+      )
+  )
 })
 
-promise.catch(err => {
+promise.catch((err) => {
   console.error(err.stack) // eslint-disable-line no-console
   process.exit(1)
 })
